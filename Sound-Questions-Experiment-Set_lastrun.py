61fcf96e53ac00a4c5944e975e60239be051f64c
Aaron Nakamura <63680964+aarontnakamura@users.noreply.github.com>
2021-10-27 16:43:42 +0900

    Add files via upload
    an updated version, rating 1-9, questions fully randomized.

--- Sound-Questions-Experiment-Set_lastrun.py
+++ Sound-Questions-Experiment-Set_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.4),
-    on Wed Oct 20 16:21:47 2021
+    on Wed Oct 27 16:40:44 2021
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -86,38 +86,25 @@ soundstimuli = sound.Sound('A', secs=-1.0, stereo=True, hamming=True,
     name='soundstimuli')
 soundstimuli.setVolume(1.0)
 
-# Initialize components for Routine "Question1_Happy"
-Question1_HappyClock = core.Clock()
-Happy_rating_text = visual.TextStim(win=win, name='Happy_rating_text',
-    text='幸福感の度合いを教えてください\xa0\n(１＝低い、７＝高い）',
-    font=None,
-    pos=(0, 0), height=0.04, wrapWidth=None, ori=0.0, 
+# Initialize components for Routine "question"
+questionClock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text='',
+    font='Open Sans',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
     color='black', colorSpace='rgb', opacity=None, 
     languageStyle='LTR',
     depth=0.0);
-Happy_rating = visual.RatingScale(win=win, name='Happy_rating', marker='triangle', size=1.0, pos=[0.0, -0.4], choices=['1', '2', '3', '4', '5', '6', '7'], tickHeight=-1)
+rating = visual.RatingScale(win=win, name='rating', marker='triangle', size=1.0, pos=[0.0, -0.4], choices=['1', '2', '3', '4', '5', '6', '7', '8', '9'], tickHeight=-1)
+
+# Initialize components for Routine "Question1_Happy"
+Question1_HappyClock = core.Clock()
 
 # Initialize components for Routine "Querstion2_Sad"
 Querstion2_SadClock = core.Clock()
-Sad_rating_text = visual.TextStim(win=win, name='Sad_rating_text',
-    text='悲壮感の度合いを教えてください\xa0\n(１＝低い、７＝高い）',
-    font=None,
-    pos=(0, 0), height=0.04, wrapWidth=None, ori=0.0, 
-    color='black', colorSpace='rgb', opacity=None, 
-    languageStyle='LTR',
-    depth=0.0);
-Sad_rating = visual.RatingScale(win=win, name='Sad_rating', marker='triangle', size=1.0, pos=[0.0, -0.4], choices=['1', '2', '3', '4', '5', '6', '7'], tickHeight=-1)
 
 # Initialize components for Routine "Queastion3_Joy"
 Queastion3_JoyClock = core.Clock()
-Joy_rating_text = visual.TextStim(win=win, name='Joy_rating_text',
-    text='喜楽感の度合いを教えてください\xa0\n(１＝低い、７＝高い）',
-    font=None,
-    pos=(0, 0), height=0.04, wrapWidth=None, ori=0.0, 
-    color='black', colorSpace='rgb', opacity=None, 
-    languageStyle='LTR',
-    depth=0.0);
-Joy_rating = visual.RatingScale(win=win, name='Joy_rating', marker='triangle', size=1.0, pos=[0.0, -0.4], choices=['1', '2', '3', '4', '5', '6', '7'], tickHeight=-1)
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -213,9 +200,9 @@ for thisQuestion_loop in question_loop:
     question_loop.addData('soundstimuli.stopped', soundstimuli.tStopRefresh)
     
     # set up handler to look after randomisation of conditions etc
-    trials = data.TrialHandler(nReps=1.0, method='random', 
+    trials = data.TrialHandler(nReps=1.0, method='fullRandom', 
         extraInfo=expInfo, originPath=-1,
-        trialList=[None],
+        trialList=data.importConditions('Question-sets_loop.xlsx'),
         seed=None, name='trials')
     thisExp.addLoop(trials)  # add the loop to the experiment
     thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
@@ -231,12 +218,112 @@ for thisQuestion_loop in question_loop:
             for paramName in thisTrial:
                 exec('{} = thisTrial[paramName]'.format(paramName))
         
+        # ------Prepare to start Routine "question"-------
+        continueRoutine = True
+        # update component parameters for each repeat
+        text.setText(text_box)
+        rating.reset()
+        # keep track of which components have finished
+        questionComponents = [text, rating]
+        for thisComponent in questionComponents:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        questionClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+        frameN = -1
+        
+        # -------Run Routine "question"-------
+        while continueRoutine:
+            # get current time
+            t = questionClock.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=questionClock)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            
+            # *text* updates
+            if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                text.frameNStart = frameN  # exact frame index
+                text.tStart = t  # local t and not account for scr refresh
+                text.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+                text.setAutoDraw(True)
+            # *rating* updates
+            if rating.status == NOT_STARTED and t >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                rating.frameNStart = frameN  # exact frame index
+                rating.tStart = t  # local t and not account for scr refresh
+                rating.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(rating, 'tStartRefresh')  # time at next scr refresh
+                rating.setAutoDraw(True)
+            continueRoutine &= rating.noResponse  # a response ends the trial
+            
+            # check for quit (typically the Esc key)
+            if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+                core.quit()
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in questionComponents:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # -------Ending Routine "question"-------
+        for thisComponent in questionComponents:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        trials.addData('text.started', text.tStartRefresh)
+        trials.addData('text.stopped', text.tStopRefresh)
+        # store data for trials (TrialHandler)
+        trials.addData('rating.response', rating.getRating())
+        trials.addData('rating.rt', rating.getRT())
+        trials.addData('rating.started', rating.tStart)
+        trials.addData('rating.stopped', rating.tStop)
+        # the Routine "question" was not non-slip safe, so reset the non-slip timer
+        routineTimer.reset()
+        thisExp.nextEntry()
+        
+    # completed 1.0 repeats of 'trials'
+    
+    
+    # set up handler to look after randomisation of conditions etc
+    inner_loop = data.TrialHandler(nReps=1.0, method='random', 
+        extraInfo=expInfo, originPath=-1,
+        trialList=[None],
+        seed=None, name='inner_loop')
+    thisExp.addLoop(inner_loop)  # add the loop to the experiment
+    thisInner_loop = inner_loop.trialList[0]  # so we can initialise stimuli with some values
+    # abbreviate parameter names if possible (e.g. rgb = thisInner_loop.rgb)
+    if thisInner_loop != None:
+        for paramName in thisInner_loop:
+            exec('{} = thisInner_loop[paramName]'.format(paramName))
+    
+    for thisInner_loop in inner_loop:
+        currentLoop = inner_loop
+        # abbreviate parameter names if possible (e.g. rgb = thisInner_loop.rgb)
+        if thisInner_loop != None:
+            for paramName in thisInner_loop:
+                exec('{} = thisInner_loop[paramName]'.format(paramName))
+        
         # ------Prepare to start Routine "Question1_Happy"-------
         continueRoutine = True
         # update component parameters for each repeat
-        Happy_rating.reset()
         # keep track of which components have finished
-        Question1_HappyComponents = [Happy_rating_text, Happy_rating]
+        Question1_HappyComponents = []
         for thisComponent in Question1_HappyComponents:
             thisComponent.tStart = None
             thisComponent.tStop = None
@@ -259,24 +346,6 @@ for thisQuestion_loop in question_loop:
             frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
             # update/draw components on each frame
             
-            # *Happy_rating_text* updates
-            if Happy_rating_text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Happy_rating_text.frameNStart = frameN  # exact frame index
-                Happy_rating_text.tStart = t  # local t and not account for scr refresh
-                Happy_rating_text.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Happy_rating_text, 'tStartRefresh')  # time at next scr refresh
-                Happy_rating_text.setAutoDraw(True)
-            # *Happy_rating* updates
-            if Happy_rating.status == NOT_STARTED and t >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Happy_rating.frameNStart = frameN  # exact frame index
-                Happy_rating.tStart = t  # local t and not account for scr refresh
-                Happy_rating.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Happy_rating, 'tStartRefresh')  # time at next scr refresh
-                Happy_rating.setAutoDraw(True)
-            continueRoutine &= Happy_rating.noResponse  # a response ends the trial
-            
             # check for quit (typically the Esc key)
             if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
                 core.quit()
@@ -298,22 +367,14 @@ for thisQuestion_loop in question_loop:
         for thisComponent in Question1_HappyComponents:
             if hasattr(thisComponent, "setAutoDraw"):
                 thisComponent.setAutoDraw(False)
-        trials.addData('Happy_rating_text.started', Happy_rating_text.tStartRefresh)
-        trials.addData('Happy_rating_text.stopped', Happy_rating_text.tStopRefresh)
-        # store data for trials (TrialHandler)
-        trials.addData('Happy_rating.response', Happy_rating.getRating())
-        trials.addData('Happy_rating.rt', Happy_rating.getRT())
-        trials.addData('Happy_rating.started', Happy_rating.tStart)
-        trials.addData('Happy_rating.stopped', Happy_rating.tStop)
         # the Routine "Question1_Happy" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
         
         # ------Prepare to start Routine "Querstion2_Sad"-------
         continueRoutine = True
         # update component parameters for each repeat
-        Sad_rating.reset()
         # keep track of which components have finished
-        Querstion2_SadComponents = [Sad_rating_text, Sad_rating]
+        Querstion2_SadComponents = []
         for thisComponent in Querstion2_SadComponents:
             thisComponent.tStart = None
             thisComponent.tStop = None
@@ -336,24 +397,6 @@ for thisQuestion_loop in question_loop:
             frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
             # update/draw components on each frame
             
-            # *Sad_rating_text* updates
-            if Sad_rating_text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Sad_rating_text.frameNStart = frameN  # exact frame index
-                Sad_rating_text.tStart = t  # local t and not account for scr refresh
-                Sad_rating_text.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Sad_rating_text, 'tStartRefresh')  # time at next scr refresh
-                Sad_rating_text.setAutoDraw(True)
-            # *Sad_rating* updates
-            if Sad_rating.status == NOT_STARTED and t >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Sad_rating.frameNStart = frameN  # exact frame index
-                Sad_rating.tStart = t  # local t and not account for scr refresh
-                Sad_rating.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Sad_rating, 'tStartRefresh')  # time at next scr refresh
-                Sad_rating.setAutoDraw(True)
-            continueRoutine &= Sad_rating.noResponse  # a response ends the trial
-            
             # check for quit (typically the Esc key)
             if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
                 core.quit()
@@ -375,22 +418,14 @@ for thisQuestion_loop in question_loop:
         for thisComponent in Querstion2_SadComponents:
             if hasattr(thisComponent, "setAutoDraw"):
                 thisComponent.setAutoDraw(False)
-        trials.addData('Sad_rating_text.started', Sad_rating_text.tStartRefresh)
-        trials.addData('Sad_rating_text.stopped', Sad_rating_text.tStopRefresh)
-        # store data for trials (TrialHandler)
-        trials.addData('Sad_rating.response', Sad_rating.getRating())
-        trials.addData('Sad_rating.rt', Sad_rating.getRT())
-        trials.addData('Sad_rating.started', Sad_rating.tStart)
-        trials.addData('Sad_rating.stopped', Sad_rating.tStop)
         # the Routine "Querstion2_Sad" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
         
         # ------Prepare to start Routine "Queastion3_Joy"-------
         continueRoutine = True
         # update component parameters for each repeat
-        Joy_rating.reset()
         # keep track of which components have finished
-        Queastion3_JoyComponents = [Joy_rating_text, Joy_rating]
+        Queastion3_JoyComponents = []
         for thisComponent in Queastion3_JoyComponents:
             thisComponent.tStart = None
             thisComponent.tStop = None
@@ -413,24 +448,6 @@ for thisQuestion_loop in question_loop:
             frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
             # update/draw components on each frame
             
-            # *Joy_rating_text* updates
-            if Joy_rating_text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Joy_rating_text.frameNStart = frameN  # exact frame index
-                Joy_rating_text.tStart = t  # local t and not account for scr refresh
-                Joy_rating_text.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Joy_rating_text, 'tStartRefresh')  # time at next scr refresh
-                Joy_rating_text.setAutoDraw(True)
-            # *Joy_rating* updates
-            if Joy_rating.status == NOT_STARTED and t >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Joy_rating.frameNStart = frameN  # exact frame index
-                Joy_rating.tStart = t  # local t and not account for scr refresh
-                Joy_rating.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Joy_rating, 'tStartRefresh')  # time at next scr refresh
-                Joy_rating.setAutoDraw(True)
-            continueRoutine &= Joy_rating.noResponse  # a response ends the trial
-            
             # check for quit (typically the Esc key)
             if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
                 core.quit()
@@ -452,18 +469,11 @@ for thisQuestion_loop in question_loop:
         for thisComponent in Queastion3_JoyComponents:
             if hasattr(thisComponent, "setAutoDraw"):
                 thisComponent.setAutoDraw(False)
-        trials.addData('Joy_rating_text.started', Joy_rating_text.tStartRefresh)
-        trials.addData('Joy_rating_text.stopped', Joy_rating_text.tStopRefresh)
-        # store data for trials (TrialHandler)
-        trials.addData('Joy_rating.response', Joy_rating.getRating())
-        trials.addData('Joy_rating.rt', Joy_rating.getRT())
-        trials.addData('Joy_rating.started', Joy_rating.tStart)
-        trials.addData('Joy_rating.stopped', Joy_rating.tStop)
         # the Routine "Queastion3_Joy" was not non-slip safe, so reset the non-slip timer
         routineTimer.reset()
         thisExp.nextEntry()
         
-    # completed 1.0 repeats of 'trials'
+    # completed 1.0 repeats of 'inner_loop'
     
     thisExp.nextEntry()
     
